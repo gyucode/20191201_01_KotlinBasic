45e04ebe0b41bc6ca605ae7be3e1fa9e748def86
gyucode <gyucode@gmail.com>
2019-12-01 11:27:03 +0900

    로그 출력 연습

--- app/src/main/java/kr/co/tjoeun/a20191201_01_kotlinbasic/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191201_01_kotlinbasic/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.co.tjoeun.a20191201_01_kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +19,17 @@ class MainActivity : AppCompatActivity() {
         okBtn.setOnClickListener {
             var inputString = inputEdt.text.toString() //get => 내용물 받아오기
             resultTxt.text = inputString //set => 내용물 설정하기.
+
+//            확인버튼이 눌렸는지 로그
+            Log.d("확인", inputString)
         }
-        okBtn.
+
+
         okBtn.setOnLongClickListener {
 
             resultTxt.text = "초기상태"
 //            onClick무시.,return true
+            Log.d("확인버튼 롱클릭", "텍스트뷰초기화")
             return@setOnLongClickListener true
         }
 
